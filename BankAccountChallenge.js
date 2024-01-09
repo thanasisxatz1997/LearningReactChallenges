f0115762a0e34e00cffba8de06aa67e04a1635c3
thanasisxatz1997 <93034841+thanasisxatz1997@users.noreply.github.com>
2024-01-09 15:50:04 +0200

    Create BankAccountChallenge.js

---
+++ BankAccountChallenge.js
@@ -0,0 +1,100 @@
+import "./styles.css";
+import { useReducer } from "react";
+
+const initialState = { balance: 0, loan: 0, status: "closed" };
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "openAccount":
+      return { ...state, balance: 400, status: "open" };
+    case "deposit":
+      return {
+        ...state,
+        balance: state.balance + action.payload,
+      };
+    case "withdraw":
+      return state.balance >= action.payload
+        ? { ...state, balance: state.balance - action.payload }
+        : { ...state };
+    case "closeAccount":
+      return state.status === "open" && state.balance === 0 && state.loan === 0
+        ? {
+            ...state,
+            status: "closed",
+          }
+        : { ...state };
+    case "requestLoan":
+      return state.loan === 0 ? { ...state, loan: 5000 } : { ...state };
+    case "payLoan":
+      return state.balance >= state.loan
+        ? { ...state, balance: state.balance - state.loan, loan: 0 }
+        : { ...state };
+    default:
+      throw new Error("err");
+  }
+}
+
+export default function BankAcountChallenge() {
+  const [{ balance, loan, status }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+  const isOpen = status === "open";
+
+  return (
+    <div className="App">
+      <h1>useReducer Bank Account</h1>
+      <p>Balance: {balance}</p>
+      <p>Loan: {loan}</p>
+
+      <p>
+        <button
+          onClick={() => dispatch({ type: "openAccount" })}
+          disabled={isOpen}
+        >
+          Open account
+        </button>
+      </p>
+      <p>
+        <button
+          onClick={() => dispatch({ type: "deposit", payload: 150 })}
+          disabled={!isOpen}
+        >
+          Deposit 150
+        </button>
+      </p>
+      <p>
+        <button
+          onClick={() => dispatch({ type: "withdraw", payload: 50 })}
+          disabled={!isOpen}
+        >
+          Withdraw 50
+        </button>
+      </p>
+      <p>
+        <button
+          onClick={() => dispatch({ type: "requestLoan", payload: 5000 })}
+          disabled={!isOpen}
+        >
+          Request a loan of 5000
+        </button>
+      </p>
+      <p>
+        <button
+          onClick={() => dispatch({ type: "payLoan" })}
+          disabled={!isOpen}
+        >
+          Pay loan
+        </button>
+      </p>
+      <p>
+        <button
+          onClick={() => dispatch({ type: "closeAccount" })}
+          disabled={!isOpen}
+        >
+          Close account
+        </button>
+      </p>
+    </div>
+  );
+}
